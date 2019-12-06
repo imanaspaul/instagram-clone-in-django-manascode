2c343c5695f9adc4d8f2316a73c8e6901fbf4db9
Manas <imanaspaul@gmail.com>
2019-12-06 19:40:20 +0530

    Profile Model created :tada:

--- server/userprofile/models.py
+++ server/userprofile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Profile Model
+
+class Profile(models.Model):
+
+    fullname = models.CharField(max_length=200)
+    dob = models.DateTimeField(verbose_name='Date of Birth', auto_now_add=False)
+    picture = models.CharField(max_length=1000) # we are going to use firestore to store our images
+    bio = models.TextField(verbose_name="About yourself")
+    website = models.URLField(blank=True, null=True)
+    phonenumber = models.TextField(blank=True, null=True)
+    gender = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fullname
+    
+    class Meta:
+        verbose_name_plural = "Profiles"
