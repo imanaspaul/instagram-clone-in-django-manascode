97e3c4eda5ca067edc0e7bad90c0698bceefed0c
Manas <imanaspaul@gmail.com>
2019-12-06 19:48:55 +0530

    corrected the user model

--- server/userprofile/models.py
+++ server/userprofile/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Profile Model
 
 class Profile(models.Model):
@@ -9,8 +9,10 @@ class Profile(models.Model):
     picture = models.CharField(max_length=1000) # we are going to use firestore to store our images
     bio = models.TextField(verbose_name="About yourself")
     website = models.URLField(blank=True, null=True)
-    phonenumber = models.TextField(blank=True, null=True)
-    gender = models.TextField(blank=True, null=True)
+    phonenumber = models.CharField(max_length=50, blank=True, null=True)
+    gender = models.CharField(max_length=50, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=True)
+
 
     def __str__(self):
         return self.fullname
